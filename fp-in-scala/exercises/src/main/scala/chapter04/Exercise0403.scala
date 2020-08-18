51af44ee7361810c56b8fdd5a8008d7f49d05ee1
Timo Denk <github@timodenk.com>
2020-08-18 21:30:57 +0200

    Add exercise 4.3

---
+++ fp-in-scala/exercises/src/main/scala/chapter04/Exercise0403.scala
@@ -0,0 +1,19 @@
+package chapter04
+
+object Exercise0403 {
+  /*
+  What is the relationship of b.map(btoC) and lift?
+  Found this one comparably tricky, even though the solution ended up being simple.
+ */
+  def curry[A,B,C](f: (A, B) => C): A => B => C = a => b => f(a, b)
+
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
+
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+
+    val curriedF = curry(f)
+    val partialF: Option[B => C] = a.map(curriedF)
+    def applyBtoC(btoC: B => C): Option[C] = b.map(btoC)
+    partialF.flatMap(applyBtoC)
+  }
+}
