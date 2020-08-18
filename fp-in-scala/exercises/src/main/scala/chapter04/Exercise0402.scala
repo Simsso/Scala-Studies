6169b1aad8f302b300ce63ea0ac8d7861fcbfafb
Timo Denk <github@timodenk.com>
2020-08-18 20:57:33 +0200

    Add exercise 4.2 (partly)

---
+++ fp-in-scala/exercises/src/main/scala/chapter04/Exercise0402.scala
@@ -0,0 +1,11 @@
+package chapter04
+
+object Exercise0402 {
+  def variance(xs: Seq[Double]): Option[Double] = {
+    val m: Option[Double] = if (xs.isEmpty) None else Option(xs.sum / xs.length)
+    val perElementVariance: Seq[Option[Double]] = xs.map(x => m.flatMap(m => math.pow(x - m, 2)) )
+    val meanVariance = ???
+  }
+}
+
+
