c5c7cf22f7dcf56b87802036d916f3851ba5785a
Timo Denk <github@timodenk.com>
2020-08-19 08:24:17 +0200

    Add exercise 4.4

--- fp-in-scala/exercises/src/main/scala/chapter04/Exercise0401.scala
+++ fp-in-scala/exercises/src/main/scala/chapter04/Exercise0401.scala
@@ -4,34 +4,34 @@ class Exercise0401 {
 
 }
 
-trait Option[+A] {
-  def map[B](f: A => B): Option[B]
-  def flatMap[B](f: A => Option[B]): Option[B]
+trait MyOption[+A] {
+  def map[B](f: A => B): MyOption[B]
+  def flatMap[B](f: A => MyOption[B]): MyOption[B]
   def getOrElse[B >: A](default: => B): B
-  def orElse[B >: A](ob: => Option[B]): Option[B]
-  def filter(f: A => Boolean): Option[A]
+  def orElse[B >: A](ob: => MyOption[B]): MyOption[B]
+  def filter(f: A => Boolean): MyOption[A]
 }
 
-case class Some[+A](get: A) extends Option[A] {
-  override def map[B](f: A => B): Option[B] = Some(f(this.get))
+case class MySome[+A](get: A) extends MyOption[A] {
+  override def map[B](f: A => B): MyOption[B] = MySome(f(this.get))
 
-  override def flatMap[B](f: A => Option[B]): Option[B] = f(this.get)
+  override def flatMap[B](f: A => MyOption[B]): MyOption[B] = f(this.get)
 
   override def getOrElse[B >: A](default: => B): B = this.get
 
-  override def orElse[B >: A](ob: => Option[B]): Option[B] = this
+  override def orElse[B >: A](ob: => MyOption[B]): MyOption[B] = this
 
-  override def filter(f: A => Boolean): Option[A] = if (f(this.get)) this else None
+  override def filter(f: A => Boolean): MyOption[A] = if (f(this.get)) this else MyNone
 }
 
-case object None extends Option[Nothing] {
-  override def map[B](f: Nothing => B): Option[B] = this
+case object MyNone extends MyOption[Nothing] {
+  override def map[B](f: Nothing => B): MyOption[B] = this
 
-  override def flatMap[B](f: Nothing => Option[B]): Option[B] = this
+  override def flatMap[B](f: Nothing => MyOption[B]): MyOption[B] = this
 
   override def getOrElse[B >: Nothing](default: => B): B = default
 
-  override def orElse[B >: Nothing](ob: => Option[B]): Option[B] = ob
+  override def orElse[B >: Nothing](ob: => MyOption[B]): MyOption[B] = ob
 
-  override def filter(f: Nothing => Boolean): Option[Nothing] = this
+  override def filter(f: Nothing => Boolean): MyOption[Nothing] = this
 }
---
+++ fp-in-scala/exercises/src/main/scala/chapter04/Exercise0404.scala
@@ -0,0 +1,24 @@
+package chapter04
+
+object Exercise0404 {
+  def sequence1[A](a: List[Option[A]]): Option[List[A]] = {
+    def aggr(xs: Option[List[A]], x: Option[A]): Option[List[A]] = {
+      if (xs == None) None
+      else if (x == None) None
+      else {
+        def prepend(l: List[A]) = x :: l
+        xs.flatMap(prepend)
+      }
+    }
+    a.foldLeft(Some(List()))(aggr)
+  }
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    def aggr(nextElem: Option[A], accumulator: Option[List[A]]): Option[List[A]] = {
+      if (nextElem.isEmpty || accumulator.isEmpty) None
+      else Some(nextElem.get :: accumulator.get)
+    }
+    val z: Option[List[A]] = Some(List())
+    a.foldRight(z)(aggr)
+  }
+}
