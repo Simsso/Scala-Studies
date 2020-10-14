904f6697d57c1e823b40fcc4fab3167eb4b6942e
Timo Denk <github@timodenk.com>
2020-10-14 12:58:11 +0200

    Add exercise 5.12

--- fp-in-scala/exercises/src/main/scala/chapter05/Exercise0511.scala
+++ fp-in-scala/exercises/src/main/scala/chapter05/Exercise0511.scala
@@ -1,12 +1,10 @@
 package chapter05
 
-import java.util.Optional
-
 class Exercise0511 {
-  def unfold[A, S](z: S)(f: S => Optional[(A, S)]): Stream[A] = {
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
     f(z) match {
-      case Right(a, s) => Stream.cons(a, unfold(s))
-      case Nil => Nil
+      case Some((a, s)) => Stream.cons(a, unfold(s))
+      case None => Stream.empty
     }
   }
 }
